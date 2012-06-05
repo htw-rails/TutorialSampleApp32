5a739fcdc0cd1748ff75f76e4ef01cae96cb6302
B Kleinen <kleinen@infrastructure.de>
2012-06-05 15:38:55 +0200

    forgot to save some files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  # Return a title on a per-page basis.
+  def title
+    if @title.nil?
+      "Tutorial, an unknown Page"
+    else
+       "Tutorial, the #{@title} Page"
+    end
+  end
+  
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
