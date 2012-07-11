9d39ea04076d3cfcac5d5434303d5c5dddb46aae
B Kleinen <kleinen@infrastructure.de>
2012-07-12 01:43:28 +0200

    changed feed to include replies

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
 
   def feed
-    Micropost.from_users_followed_by(self)
+    Micropost.from_users_followed_by_including_replies(self)
   end
 
   def has_password?(submitted_password)
